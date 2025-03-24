e7ee9d923a1b2fe9e4e633f287596dac5b810270
diegorem <diegorem99@gmail.com>
2025-03-23 19:36:54 -0600

    15

---
+++ 15_map_dicts.py
@@ -0,0 +1,17 @@
+items = [
+    {
+        'product': 'camisa',
+        'price': 100
+    },
+    {
+        'product': 'pantalones',
+        'price': 300
+    },
+    {
+        'product': 'pantalones 2',
+        'price': 200
+    }
+]
+
+prices = list(map(lambda item: item['price'], items))
+print(prices)
